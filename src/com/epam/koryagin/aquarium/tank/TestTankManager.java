0d2aa7cfa124f19e826272c57600852b5a0a9322
alhenk <alexandr.g.koryagin@gmail.com>
2014-02-03 10:01:18 +0600

    implemented junit test class, how to run?

--- src/com/epam/koryagin/aquarium/tank/TestTankManager.java
+++ src/com/epam/koryagin/aquarium/tank/TestTankManager.java
@@ -1,17 +1,47 @@
 package com.epam.koryagin.aquarium.tank;
 
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestTankManager {
+import com.epam.koryagin.aquarium.accessory.Accessory;
+import com.epam.koryagin.aquarium.accessory.AccessoryType;
+import com.epam.koryagin.aquarium.animal.Fish;
+import com.epam.koryagin.aquarium.animal.FishType;
+import com.epam.koryagin.aquarium.animal.Reptile;
+import com.epam.koryagin.aquarium.animal.ReptileType;
 
+public class TestTankManager{
+	private static Tank tank;
+	private static Accessory accessory;
+	private static Fish fish;
+	private static Reptile reptile;
+	private static TankManager tm;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		tank = new Tank();
+		tank.setName("Community tank");
+		tank.setDescription(TankType.COMMUNITY_TANK.getDescription());
+		tank.setVolume(30.0);
+		tank.setCost(123.5);
+		
+		accessory = new Accessory();
+		accessory.setName("Algea");
+		accessory.setDescription(AccessoryType.ALGEA.getDescription());
+		accessory.setCost(5.36);
+		
+		fish = new Fish();
+		fish.setName("Basslet");
+		fish.setTaxonomy(FishType.ANGELFISH.getTaxonomy());
+		fish.setCost(3.25);
+		
+		reptile = new Reptile();
+		reptile.setName("Frog");
+		reptile.setTaxonomy(ReptileType.FROG.getTaxonomy());
+		reptile.setCost(12.5);
 	}
 
 	@AfterClass
@@ -20,6 +50,9 @@ public class TestTankManager {
 
 	@Before
 	public void setUp() throws Exception {
+		tank.addAccessory(accessory);
+		tank.addAnimal(fish);
+		tank.addAnimal(reptile);
 	}
 
 	@After
@@ -28,7 +61,9 @@ public class TestTankManager {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		double result = tm.calculateTotalSum(tank);
+		assertEquals("Total must be 139.61",139.61,result,0.005); 
+		//fail("Not yet implemented");
 	}
 
 }
